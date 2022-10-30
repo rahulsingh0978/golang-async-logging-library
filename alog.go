9b51ba2e23c1487a4abd41f46f3df8b24cf1d257
Rahul Singh <rsingh4@salesforce.com>
2022-10-30 22:03:55 +0530

    .

--- alog.go
+++ alog.go
@@ -32,6 +32,7 @@ func New(w io.Writer) *Alog {
 		dest:    w,
 		msgCh:   make(chan string),
 		errorCh: make(chan error),
+		m:       &sync.Mutex{},
 	}
 }
 
@@ -49,6 +50,14 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	al.m.Lock()
+	defer al.m.Unlock()
+	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
+	if err != nil {
+		go func(er error) {
+			al.errorCh <- er
+		}(err)
+	}
 }
 
 func (al Alog) shutdown() {
