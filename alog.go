4998f7420e0278bf6786cc86ad024e21e1ccbadf
Rahul Singh <rsingh4@salesforce.com>
2022-10-30 22:05:11 +0530

    .

--- alog.go
+++ alog.go
@@ -54,9 +54,10 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 	defer al.m.Unlock()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
 	if err != nil {
-		go func(er error) {
+		panic("this is error")
+		/*go func(er error) {
 			al.errorCh <- er
-		}(err)
+		}(err)*/
 	}
 }
 
